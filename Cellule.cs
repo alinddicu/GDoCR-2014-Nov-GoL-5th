7f6581d4a951795e17185ef2d140e7e508e08e39
alinddicu <alinddicu@yahoo.com>
2014-11-17 21:24:22 +0100

    init

---
+++ Cellule.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GoL
+{
+    public class Cellule
+    {
+        public EtatCellule Etat { get; private set; }
+
+        private int _nbVoisinsVivants;
+
+        public event Action NaissanceEvent;
+
+        public event Action MortEvent;
+
+        public Cellule(EtatCellule etat = EtatCellule.Mort)
+        {
+            Etat = etat;
+        }
+
+        private void Cellule_NaissanceEvent()
+        {
+            _nbVoisinsVivants++;
+        }
+
+        private void Cellule_MortEvent()
+        {
+            _nbVoisinsVivants--;
+        }
+
+        public void AjouterVoisinVivant(Cellule cellule)
+        {
+            _nbVoisinsVivants++;
+            cellule.NaissanceEvent += Cellule_NaissanceEvent;
+            cellule.MortEvent += Cellule_MortEvent;
+        }
+
+        public void AjouterVoisinMort(Cellule cellule)
+        {
+            cellule.NaissanceEvent += Cellule_NaissanceEvent;
+            cellule.MortEvent += Cellule_MortEvent;
+        }
+
+        public void Muter()
+        {
+            var nouvelEtat = EtatCellule.Mort;
+            if (Etat == EtatCellule.Vivant && (_nbVoisinsVivants == 2 || _nbVoisinsVivants == 3))
+            {
+                nouvelEtat = EtatCellule.Vivant;
+            }
+            else if (Etat == EtatCellule.Mort && _nbVoisinsVivants == 3)
+            {
+                nouvelEtat = EtatCellule.Vivant;
+            }
+
+            if (Etat != nouvelEtat)
+            {
+                Etat = nouvelEtat;
+                if (Etat == EtatCellule.Vivant)
+                {
+                    NaissanceEvent();
+                }
+                else
+                {
+                    MortEvent();
+                }
+            }
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GoL
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void EtantDonneUneCelluleVivanteEt2VoisinesVivantesQuandElleMuteElleResteEnVie()
+        {
+            var cellule1 = new Cellule(EtatCellule.Vivant);
+            var cellule2 = new Cellule(EtatCellule.Vivant);
+            var cellule3 = new Cellule(EtatCellule.Vivant);
+            var cellule4 = new Cellule();
+
+            cellule1.AjouterVoisinVivant(cellule2);
+            cellule1.AjouterVoisinVivant(cellule3);
+            cellule1.AjouterVoisinMort(cellule4);
+
+            cellule2.AjouterVoisinVivant(cellule1);
+            cellule2.AjouterVoisinVivant(cellule3);
+            cellule2.AjouterVoisinMort(cellule4);
+
+            cellule3.AjouterVoisinVivant(cellule1);
+            cellule3.AjouterVoisinVivant(cellule2);
+            cellule3.AjouterVoisinMort(cellule4);
+
+            cellule4.AjouterVoisinVivant(cellule1);
+            cellule4.AjouterVoisinVivant(cellule2);
+            cellule4.AjouterVoisinVivant(cellule3);
+
+            cellule1.Muter();
+            cellule2.Muter();
+            cellule3.Muter();
+            cellule4.Muter();
+
+            Assert.IsTrue(cellule1.Etat == EtatCellule.Vivant);
+            Assert.IsTrue(cellule2.Etat == EtatCellule.Vivant);
+            Assert.IsTrue(cellule3.Etat == EtatCellule.Vivant);
+            Assert.IsTrue(cellule4.Etat == EtatCellule.Vivant);
+
+            cellule1.Muter();
+            cellule2.Muter();
+            cellule3.Muter();
+            cellule4.Muter();
+
+            Assert.IsTrue(cellule1.Etat == EtatCellule.Vivant);
+            Assert.IsTrue(cellule2.Etat == EtatCellule.Vivant);
+            Assert.IsTrue(cellule3.Etat == EtatCellule.Vivant);
+            Assert.IsTrue(cellule4.Etat == EtatCellule.Vivant);
+        }
+    }
+}
